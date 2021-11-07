8dffd4325a3cb326e3c1a017b6ac2c5003e77633
Ex1Kx <thenewestekigoh@gmail.com>
2021-11-06 20:04:27 -0500

    [Ex1] Added The 2nd Part In This Proyect

--- producto/producto.component.ts
+++ producto/producto.component.ts
@@ -6,6 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./producto.component.css']
 })
 export class ProductoComponent implements OnInit {
+  /*Primera Parte De la Practica
   public nombre_componentes = "Componente Productos";
   public listado_productos = "Zapatos, Tenis, Ropa";
   public nombre:string = "Julian Andres Lozada Valencia"
@@ -15,7 +16,23 @@ export class ProductoComponent implements OnInit {
   public arreglo1: number[] = [1, 2, 3];
   public arreglo2:Array<string> = ['first', '2nd', '3rd'];
   public varios:Array<any> = [1, 'Julian', true];
-  constructor() { }
+  */
+ // Segunda Parte De La Practica
+  public nombre_componente:string;
+  public listado_productos:string;
+  public arreglo2:Array<string>;
+  public varios:Array<any>;
+
+  constructor() {
+    this.nombre_componente = "Componente De Producto";
+    this.listado_productos = 'Zapatos, Tenis, Botas';
+    this.arreglo2 = ['PHP', 'Java', 'Python'];
+    this.varios = [1,'Julian',true];
+    this.saludo();
+   }
+   saludo(){
+     alert("Esto es un saludo");
+   }
 
   ngOnInit(): void {
   }
