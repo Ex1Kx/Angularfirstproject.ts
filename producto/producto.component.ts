7f21e173f2b92caa6d3bd9a091bb5e75dc77ba6f
Ex1Kx <thenewestekigoh@gmail.com>
2021-11-06 21:48:08 -0500

    [Ex1] Added The 3rd Part In This Project

--- producto/producto.component.ts
+++ producto/producto.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, NgModule } from '@angular/core';
+import { Producto } from './producto';
 
 @Component({
   selector: 'app-producto',
@@ -17,24 +18,49 @@ export class ProductoComponent implements OnInit {
   public arreglo2:Array<string> = ['first', '2nd', '3rd'];
   public varios:Array<any> = [1, 'Julian', true];
   */
- // Segunda Parte De La Practica
+ /* Segunda Parte De La Practica
   public nombre_componente:string;
   public listado_productos:string;
   public arreglo2:Array<string>;
   public varios:Array<any>;
+  */
+ // Tecera Parte De La Parctica
+  public nombre_componentes = "Componente Producto";
+  public pro!: Producto;
+  public inventario!:Array<Producto>;
 
   constructor() {
+    /* Segunda Parte De La Practica
     this.nombre_componente = "Componente De Producto";
     this.listado_productos = 'Zapatos, Tenis, Botas';
     this.arreglo2 = ['PHP', 'Java', 'Python'];
     this.varios = [1,'Julian',true];
     this.saludo();
+    */
    }
+   /* Tercera Parte De La Practica
    saludo(){
      alert("Esto es un saludo");
-   }
+   }*/
 
   ngOnInit(): void {
+    /*this.saludo();
+    var uno = 8;
+    var dos = 15;
+    if(uno == 18) {
+      let uno = 3;
+      var dos = 88;
+      console.log("Dentro del if"  + uno);
+    }
+    console.log("Fuera del if " + uno)*/
+    //Tercera Parte De La Practica
+    this.pro = new Producto('Tenis', 30, 'Caja', true);
+    this.inventario = [
+      new Producto('Zapatos', 5, 'Caja', true),
+      new Producto('Maquillaje', 3, 'Estuche', true),
+      new Producto('Boxer', 3, 'Bolsa', true),
+      new Producto('Jean', 2, 'Tula', true)
+    ];
   }
 
 }
---
+++ producto/producto.ts
@@ -0,0 +1,8 @@
+export class Producto{
+    constructor(
+      public nombrepro:string,
+      public cantidadexi:number,
+      public embalaje:string,
+      public estado:boolean
+    ){}
+}
